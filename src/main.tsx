8da6c34d7599a9deb469d07ceb67890fd8a33d2f
MAZE-borahm <amorosso@a-maze.ai>
2025-04-07 14:52:55 +0900

    chore: 사용하지 않는 컴포넌트 정리

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <div>
+      <a href='https://vite.dev' target='_blank'>
+        <img src={viteLogo} className='logo' alt='Vite logo' />
+      </a>
+      <a href='https://react.dev' target='_blank'>
+        <img src={reactLogo} className='logo react' alt='React logo' />
+      </a>
+    </div>
+    <h1>Vite + React</h1>
+    <div className='card'>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+    </div>
+    <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+  </StrictMode>
 )
