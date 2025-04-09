3089d9abe9da22b9d26907f6330de0205ac225f2
MAZE-borahm <amorosso@a-maze.ai>
2025-04-09 11:01:46 +0900

    chore: 타이포그래피 적용

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,7 @@ const App = () => {
       </div>
       <h1>Vite + React</h1>
       <div className='card'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p className='caption3-10'>올바로 2.0 법률 해결사 Allbarlaw</p>
       </div>
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
