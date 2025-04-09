bf624da03e66c4d1514166882f97cbd67f7689a1
MAZE-borahm <amorosso@a-maze.ai>
2025-04-09 10:30:14 +0900

    chore: 글로벌 scss 설정

--- vite.config.ts
+++ vite.config.ts
@@ -7,16 +7,16 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
-  // css: {
-  //   preprocessorOptions: {
-  //     scss: {
-  //       // 글로벌하게 import할 SCSS 파일 설정
-  //       additionalData: `
-  //         @use "@/styles/abstracts/variables" as *;
-  //         @use "@/styles/abstracts/colors" as *;
-  //         @use "@/styles/abstracts/mixins" as *;
-  //       `,
-  //     },
-  //   },
-  // },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 글로벌하게 import할 SCSS 파일 설정
+        additionalData: `
+          @use "@/styles/abstracts/variables" as *;
+          @use "@/styles/abstracts/colors" as *;
+          @use "@/styles/abstracts/mixins" as *;
+        `,
+      },
+    },
+  },
 })
