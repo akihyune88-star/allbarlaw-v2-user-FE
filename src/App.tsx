085bdb467cb810ea967d6641dc2f65a6b01ff4d3
MAZE-borahm <amorosso@a-maze.ai>
2025-04-09 14:18:31 +0900

    feat: 헤더 컴포넌트 배치

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-import SvgIcon from './components/SvgIcon'
-import viteLogo from '/vite.svg'
-import styles from '@/styles/test.module.scss'
+import Header from './components/header/Header'
 
 const App = () => {
   return (
     <>
-      <div className={styles.container}>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <SvgIcon name='loginRound' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <p className='caption3-10'>올바로 2.0 법률 해결사 Allbarlaw</p>
-      </div>
-      <p className={styles['mobile-only']}>올바로 2.0 법률 해결사 Allbarlaw</p>
+      <Header />
     </>
   )
 }
