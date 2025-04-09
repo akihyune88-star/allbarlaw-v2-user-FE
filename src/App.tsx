756850ca12f07f6f648b8cb3cd2a8a55d5df97e6
MAZE-borahm <amorosso@a-maze.ai>
2025-04-09 10:29:26 +0900

    chore: 절대경로 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import styles from '@/styles/test.module.scss'
 
 const App = () => {
   return (
     <>
-      <div>
+      <div className={styles.container}>
         <a href='https://vite.dev' target='_blank'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       // 글로벌하게 import할 SCSS 파일 설정
+  //       additionalData: `
+  //         @use "@/styles/abstracts/variables" as *;
+  //         @use "@/styles/abstracts/colors" as *;
+  //         @use "@/styles/abstracts/mixins" as *;
+  //       `,
+  //     },
+  //   },
+  // },
 })
