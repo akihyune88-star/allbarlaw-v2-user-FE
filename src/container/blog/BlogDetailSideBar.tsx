96272b9055d7e69c5c4bebf233bd748da3630d66
MAZE-borahm <amorosso@a-maze.ai>
2025-04-18 13:28:06 +0900

    feat : 변호사 정보 vertical 버전 3타입 컴포넌트 생성 및 적용

--- src/container/blog/BlogDetailSideBar.tsx
+++ src/container/blog/BlogDetailSideBar.tsx
@@ -1,5 +1,28 @@
-const BlogDetailSideBar = () => {
-  return <div></div>
+import AIRecommender from '@/components/aiRecommender/AIRecommender'
+import LawyerVertical from '@/components/lawyer/LawyerVertical'
+import styles from './blog-detail-sidebar.module.scss'
+
+const BlogDetailSideBar = ({ showLoading }: { showLoading: boolean }) => {
+  return (
+    <aside className={styles['blog-detail-sidebar']}>
+      <section className={styles['lawyer-section']}>
+        <LawyerVertical
+          id={1}
+          name='홍길동'
+          lawfirm='법무법인'
+          profileImage='https://cdn.goenhance.ai/user/2024/07/12/6df8872f-c15e-442f-a4df-caa520c34c77_1.jpg'
+          type={1}
+        />
+      </section>
+      {showLoading ? (
+        <div />
+      ) : (
+        <section className={styles['ai-section']}>
+          <AIRecommender />
+        </section>
+      )}
+    </aside>
+  )
 }
 
 export default BlogDetailSideBar
