c543340a7fb5c90c06035415162ccfc432ce419e
MAZE-borahm <amorosso@a-maze.ai>
2025-04-22 11:13:29 +0900

    feat : 채팅상담 요청하기 헤더 컴포넌트 공통으로 사용할 수 있도록 컨테이너 요소로 분리

--- src/pages/baroTalk/RequestBaroTalk.tsx
+++ src/pages/baroTalk/RequestBaroTalk.tsx
@@ -3,6 +3,7 @@ import StepProgressBar from '@/components/stepProgressBar'
 import CategorySelector from '@/container/baroTalk/CategorySelector'
 import ConsultationStatusSelector from '@/container/baroTalk/ConsultationStatusSelector'
 import LawyerPreferenceSelector from '@/container/baroTalk/LawyerPreferenceSelector'
+import RequestHeader from '@/container/baroTalk/RequestHeader'
 import RequestTypeSelector from '@/container/baroTalk/RequestTypeSelector'
 import { useMediaQuery } from '@/hooks/useMediaQuery'
 import styles from '@/pages/baroTalk/request-baro-talk.module.scss'
@@ -13,21 +14,30 @@ interface CategorySelection {
   subCategoryId: number | null
 }
 
-const PageHeader = () => {
-  const isMobile = useMediaQuery('(max-width: 1200px)')
-
+const renderNavButtons = (isMobile: boolean) => {
   if (isMobile) {
     return (
-      <header className={styles['page-header']}>
-        <h1>채팅상담 신청</h1>
-        <p>채팅상담 및 답변내용은 법률지식인에 공개될 수 있습니다.</p>
-      </header>
+      <nav className={styles['button-container']}>
+        <StepProgressBar steps={3} currentStep={1} className={styles['progress-bar']} />
+        <div className={styles['button-wrapper']}>
+          <Button variant='normal'>취소</Button>
+          <Button variant='fill' size='large' style={{ width: 120 }}>
+            저장 및 다음
+          </Button>
+        </div>
+      </nav>
     )
   } else {
     return (
-      <header className={styles['page-header']}>
-        <h1>법률 상담하기</h1>
-      </header>
+      <nav className={styles['button-container']}>
+        <Button variant='normal'>취소</Button>
+        <div className={styles['progress-wrapper']}>
+          <StepProgressBar steps={3} currentStep={1} className={styles['progress-bar']} />
+        </div>
+        <Button variant='fill' size='large'>
+          저장 및 다음
+        </Button>
+      </nav>
     )
   }
 }
@@ -54,37 +64,13 @@ const RequestBaroTalk = () => {
     }))
   }
 
-  const renderNavButtons = () => {
-    if (isMobile) {
-      return (
-        <nav className={styles['button-container']}>
-          <StepProgressBar steps={3} currentStep={1} className={styles['progress-bar']} />
-          <div className={styles['button-wrapper']}>
-            <Button variant='normal'>취소</Button>
-            <Button variant='fill' size='large' style={{ width: 120 }}>
-              저장 및 다음
-            </Button>
-          </div>
-        </nav>
-      )
-    } else {
-      return (
-        <nav className={styles['button-container']}>
-          <Button variant='normal'>취소</Button>
-          <div className={styles['progress-wrapper']}>
-            <StepProgressBar steps={3} currentStep={1} className={styles['progress-bar']} />
-          </div>
-          <Button variant='fill' size='large'>
-            저장 및 다음
-          </Button>
-        </nav>
-      )
-    }
-  }
-
   return (
     <div className={styles['request-baro-talk']}>
-      <PageHeader />
+      <RequestHeader
+        title='법률 상담하기'
+        mobileTitle='채팅상담 신청'
+        description='채팅상담 및 답변내용은 법률지식인에 공개될 수 있습니다.'
+      />
 
       <main className={styles['input-container']}>
         {!isMobile && <p className={styles['instruction-text']}>상담을 위해 아래 항목을 선택해주세요.</p>}
@@ -109,7 +95,7 @@ const RequestBaroTalk = () => {
           <LawyerPreferenceSelector />
         </section>
 
-        {renderNavButtons()}
+        {renderNavButtons(isMobile)}
       </main>
     </div>
   )
