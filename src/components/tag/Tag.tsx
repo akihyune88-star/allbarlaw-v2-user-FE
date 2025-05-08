d93b951749915544de5d1849acca9c19b789b5e5
MAZE-borahm <amorosso@a-maze.ai>
2025-05-08 17:01:50 +0900

    feat : 태그 컴포넌트에 onClick 함수를 프롭으로 넘겨 사용할 수 있도록 수정

--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -1,7 +1,11 @@
 import styles from '@/components/tag/tag.module.scss'
 
-const Tag = ({ tag }: { tag: string }) => {
-  return <span className={styles['tag']}>{tag}</span>
+const Tag = ({ tag, onClick }: { tag: string; onClick?: () => void }) => {
+  return (
+    <span className={styles['tag']} onClick={onClick} style={{ cursor: onClick ? 'pointer' : 'default' }}>
+      {tag}
+    </span>
+  )
 }
 
 export default Tag
