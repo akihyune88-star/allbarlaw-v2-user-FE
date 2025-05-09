9d1b2cdb5a654c4b9a197e7c6df8edf084ad508d
MAZE-borahm <amorosso@a-maze.ai>
2025-05-09 10:00:38 +0900

    feat : 전체 블로그 리스트의 컨텐츠 리스트 부분 2*2 로 배치될수 있도록 그리드 배치 진행

--- src/container/subMain/total/TotalBlogList.tsx
+++ src/container/subMain/total/TotalBlogList.tsx
@@ -1,19 +1,14 @@
+import Article from '@/components/article/Article'
 import ArticleHeader from '@/components/articleHeader/ArticleHeader'
-import Divider from '@/components/divider/Divider'
-import styles from '@/pages/subMain/total-sub-main.module.scss'
+import styles from '@/container/subMain/total/total-blog-list.module.scss'
 
 const TotalBlogList = () => {
   return (
     <div className={styles['total-blog-list']}>
-      <ArticleHeader
-        title={`변호사의 영상을 보고
-내 법률 문제의 해결방법을 찾으세요.`}
-        totalBlogCount={10}
-        recentBlogCount={10}
-        type='section'
-      />
-      <Divider />
       <ArticleHeader title='변호사의 글' totalBlogCount={10} recentBlogCount={10} type='total' />
+      <div className={styles['blog-list']}>
+        <Article type='xsmall' title='블로그 제목' content='블로그 내용' category='부동산' />
+      </div>
     </div>
   )
 }
