2b12c736b202202af24807ce188aee27d499d193
MAZE-borahm <amorosso@a-maze.ai>
2025-06-03 09:57:15 +0900

    feat : 백과사전 디테일의 용어설명 컴포넌트 작업 / PC 버전 헤더 컴포넌트 작업

--- src/container/legalTermDetail/LegalTermDefinition.tsx
+++ src/container/legalTermDetail/LegalTermDefinition.tsx
@@ -12,16 +12,16 @@ const LegalTermDefinition = ({ termId }: LegalTermDefinitionProps) => {
   return (
     <div className={styles.container}>
       <header>
-        <h3>파산선고</h3>
-        <p>declaration of bankruptcy</p>
+        <h3 className={styles['title']}>파산선고 (破産宣告)</h3>
+        <p className={styles['english-title']}>declaration of bankruptcy</p>
       </header>
-      <p>
+      <p className={styles['description']}>
         채무자의 지불불능 혹은 채무초과 등의 파산원인이 발생하여 채무자가 법원에 재산동결을 요청하고, 법원이 이를
         수용하여 채무자의 책임재산으로 파산재단을 구성하여 채권자에게 공평하게 배분하는 채무자 회생 및 파산에 관한 법률
         상의 절차(파산절차)를 개시하는 결정을 내리는 선고를 말한다.
       </p>
       <footer>
-        <div>{`출처 : 네이버 법률용어사전 > 법문북스 www.lawb.co.kr`}</div>
+        <p className={styles['source']}>{`출처 : 네이버 법률용어사전 > 법문북스 www.lawb.co.kr`}</p>
         <div className={styles['button-wrapper']}>
           <Button variant='share'>
             공유
--- src/pages/legalTermDetail/LegalTermDetail.tsx
+++ src/pages/legalTermDetail/LegalTermDetail.tsx
@@ -2,14 +2,28 @@ import { useLocation } from 'react-router-dom'
 import LegalItemWidget from '@/components/legalItemWidget/LegalItemWidget'
 import styles from './legal-term-detail.module.scss'
 import LegalTermDefinition from '@/container/legalTermDetail/LegalTermDefinition'
+import { useMediaQuery } from '@/hooks/useMediaQuery'
+
+const PcTotalContentsHeader = ({ amount }: { amount: number }) => {
+  return (
+    <div className={styles['total-contents-header']}>
+      <h3
+        className={styles['title']}
+      >{`해당 법률용어가 포함된 자료입니다.\n법률 문제를 해결 할 수 있는 글과 영상을 함께 찾아보세요.`}</h3>
+      <span className={styles['amount']}>전체 {amount.toLocaleString()}개</span>
+    </div>
+  )
+}
 
 const LegalTermDetail = () => {
   const { state } = useLocation()
+  const isMobile = useMediaQuery('(min-width: 80rem)')
   console.log(state.id)
   return (
     <main className={`sub-main-container ${styles.container}`}>
       <div className='contents-section'>
-        <LegalTermDefinition />
+        <LegalTermDefinition termId={state.id} />
+        <div>{isMobile && <PcTotalContentsHeader amount={2147} />}</div>
       </div>
       <div className='aside' style={{ width: 250, flexShrink: 0 }}>
         <LegalItemWidget title='많이 찾는 용어' />
