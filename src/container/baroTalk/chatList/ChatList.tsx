47c4556d4ca12bae1e719e676ca121ae9699769a
MAZE-BORAHM\lucyl <amorosso@a-maze.ai>
2025-07-25 15:25:19 +0900

    feat: 변호사 채팅 아이템 컴포넌트 추가 및 스타일 개선

--- src/container/baroTalk/chatList/ChatList.tsx
+++ src/container/baroTalk/chatList/ChatList.tsx
@@ -1,6 +1,43 @@
+import React from 'react'
+import { formatTimeAgo } from '@/utils/date'
 import styles from './chatList.module.scss'
+import Divider from '@/components/divider/Divider'
+import { COLOR } from '@/styles/color'
+
+type LawyerChatItemProps = {
+  name: string
+  profileImage: string
+  lastMessage: string
+  lastMessageTime: string
+  isOnline: boolean
+}
+
+const LawyerChatItem = ({ name, profileImage, lastMessage, lastMessageTime, isOnline }: LawyerChatItemProps) => {
+  return (
+    <div className={styles['lawyer-chat-item']}>
+      <div className={styles['lawyer-chat-item-profile']}>
+        <figure>
+          <img src={profileImage} alt={name} />
+        </figure>
+      </div>
+      <div className={styles['lawyer-chat-item-content']}>
+        <div className={styles['lawyer-chat-item-content-name-badge-wrap']}>
+          <div className={styles['lawyer-chat-item-content-name']}>{name} 변호사</div>
+          <span
+            className={styles['badge']}
+            style={{ '--badge-color': isOnline ? COLOR.green_01 : 'rgba(0, 0, 0, 0.7)' } as React.CSSProperties}
+          />
+        </div>
+        {/* <span /> */}
+        <div className={styles['lawyer-chat-item-content-last-message-time']}>{formatTimeAgo(lastMessageTime)}</div>
+        <div className={styles['lawyer-chat-item-content-last-message']}>{lastMessage}</div>
+      </div>
+    </div>
+  )
+}
 
 const ChatList = () => {
+  const lawyerChatList = [1, 2, 3, 4]
   return (
     <main className={styles['chat-list']}>
       <header className={styles['chat-list-header']}>
@@ -10,7 +47,23 @@ const ChatList = () => {
           <button>추가 상담하기</button>
         </div>
       </header>
-      <section>채팅리스트</section>
+      <section className={styles['chat-list-wrapper']}>
+        <div className={styles['chat-list-content']}>
+          {lawyerChatList.map((item, index) => (
+            <>
+              <LawyerChatItem
+                key={index}
+                name='김바로'
+                profileImage='https://picsum.photos/200/300'
+                lastMessage='안녕하세요'
+                lastMessageTime='2025-07-25 10:00'
+                isOnline={false}
+              />
+              {index !== lawyerChatList.length - 1 && <Divider padding={0} />}
+            </>
+          ))}
+        </div>
+      </section>
     </main>
   )
 }
