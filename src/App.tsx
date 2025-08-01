eb38710bebe3689264d3af99bea4411afc877104
MAZE-BORAHM\lucyl <amorosso@a-maze.ai>
2025-08-01 12:24:50 +0900

    feat: BoxProgressBar 컴포넌트 제거 및 헤더 스타일 수정, SCSS 변수 및 믹스인 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import Header from './container/header/Header'
-import BoxProgressBar from './components/boxProgressBar/BoxProgressBar'
 import styles from '@/styles/app.module.scss'
 import BottomNavigation from './components/bottomNavigation/BottomNavigation'
 
 const App = () => {
   return (
     <div className={styles.container}>
-      <BoxProgressBar />
       <Header />
       <div className={styles['inner-container']}>
         <Outlet />
