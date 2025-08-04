aa37a1c6a68997d35f805e71be6703c5889ecc9c
MAZE-BORAHM\lucyl <amorosso@a-maze.ai>
2025-08-04 13:23:31 +0900

    style: 변호사 프로필 컴포넌트의 스타일 수정 및 클래스 이름 변경, 반응형 디자인을 위한 새로운 스타일 추가. 변호사 배너 및 법률 사무소 정보 섹션 개선.

--- src/container/lawyer/lawyerProfile/LawyerProfile.tsx
+++ src/container/lawyer/lawyerProfile/LawyerProfile.tsx
@@ -1,6 +1,7 @@
 import Divider from '@/components/divider/Divider'
 import styles from './lawyerProfile.module.scss'
 import Tag from '@/components/tag/Tag'
+import LawyerHorizon from '@/components/lawyer/LawyerHorizon'
 
 type LawyerProfileProps = {
   discription: string
@@ -12,13 +13,27 @@ type LawyerProfileProps = {
 const LawyerProfile = ({ discription, lawyerLawfirm, lawyerAdress, tags }: LawyerProfileProps) => {
   return (
     <section className={styles['lawyer-profile']} aria-label='변호사 프로필'>
-      <blockquote className={styles['discription']}>{discription}</blockquote>
-      <div className={styles['lawyer-info']}>
-        <article className={styles['lawyer-lawfirm']}>
-          <div className={styles['lawyer-lawfirm-info']}>
-            <h3 className={styles['lawyer-lawfirm-info-name']}>{lawyerLawfirm}</h3>
-            <address className={styles['lawyer-lawfirm-info-adress']}>{lawyerAdress}</address>
-            <nav className={styles['lawyer-lawfirm-button']} aria-label='연락처 및 위치 정보'>
+      <blockquote className={styles['description']}>{discription}</blockquote>
+      <div className={styles['lawyer-banner']}>
+        <LawyerHorizon
+          name='이영희'
+          lawfirm={lawyerLawfirm}
+          profileImage='https://picsum.photos/200/300'
+          socialLink={[{ type: 'naver', link: 'https://www.naver.com' }]}
+          buttonComponent={
+            <button type='button' aria-label='변호사 연락처 보기' className={styles['barotalk-button']}>
+              바로톡
+            </button>
+          }
+          className={styles['lawyer-banner-item']}
+        />
+      </div>
+      <div className={styles['lawfirm-container']}>
+        <article className={styles['lawfirm']}>
+          <div className={styles['lawfirm__info']}>
+            <h3 className={styles['lawfirm__name']}>{lawyerLawfirm}</h3>
+            <address className={styles['lawfirm__address']}>{lawyerAdress}</address>
+            <nav className={styles['lawfirm__buttons']} aria-label='연락처 및 위치 정보'>
               <button type='button' aria-label='변호사 연락처 보기'>
                 연락처 보기
               </button>
@@ -31,7 +46,7 @@ const LawyerProfile = ({ discription, lawyerLawfirm, lawyerAdress, tags }: Lawye
         <Divider padding={14} />
         <nav className={styles['tags']} aria-label='전문 분야'>
           {tags.map(tag => (
-            <Tag key={tag.id} tag={tag.name} className={styles['tag']} />
+            <Tag key={tag.id} tag={tag.name} className={styles['tags__item']} />
           ))}
         </nav>
       </div>
