d168772a2de631ab46f123a5faaa3e36436be46d
MAZE-BORAHM\lucyl <amorosso@a-maze.ai>
2025-08-05 12:29:06 +0900

    style: 변호사 법률 지식 및 영상 섹션의 aria-label 속성의 따옴표 수정

--- src/container/lawyer/lawyerLegalKnowledge/LawyerLegalKnowledge.tsx
+++ src/container/lawyer/lawyerLegalKnowledge/LawyerLegalKnowledge.tsx
@@ -4,7 +4,7 @@ import styles from './lawyerLegalKnowledge.module.scss'
 
 const LawyerLegalKnowledge = () => {
   return (
-    <section className={styles['lawyer-legal-knowledge']} aria-label="변호사의 법률 지식">
+    <section className={styles['lawyer-legal-knowledge']} aria-label='변호사의 법률 지식'>
       <header className={styles['lawyer-legal-knowledge__header']}>
         <h3 className={styles['lawyer-legal-knowledge__title']}>변호사의 법률 지식</h3>
         <button type='button' className={styles['lawyer-legal-knowledge__button']} aria-label='변호사의 법률 지식 더보기'>
--- src/container/lawyer/lawyerVideo/LawyerVideo.tsx
+++ src/container/lawyer/lawyerVideo/LawyerVideo.tsx
@@ -4,7 +4,7 @@ import styles from './lawyerVideo.module.scss'
 
 const LawyerVideo = () => {
   return (
-    <section className={styles['lawyer-video']} aria-label="변호사의 영상">
+    <section className={styles['lawyer-video']} aria-label='변호사의 영상'>
       <header className={styles['lawyer-video__header']}>
         <h3 className={styles['lawyer-video__title']}>변호사의 영상</h3>
         <button type='button' className={styles['lawyer-video__button']} aria-label='변호사의 영상 더보기'>
