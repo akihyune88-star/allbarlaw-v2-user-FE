89fc308faa67350abc93b55fef1d545910ca9815
MAZE-BORAHM\lucyl <amorosso@a-maze.ai>
2025-08-05 12:28:50 +0900

    feat: 변호사 법률 지식 및 영상 섹션 추가, 스타일 적용

--- src/container/lawyer/lawyerLegalKnowledge/LawyerLegalKnowledge.tsx
+++ src/container/lawyer/lawyerLegalKnowledge/LawyerLegalKnowledge.tsx
@@ -1,5 +1,20 @@
+import Divider from '@/components/divider/Divider'
+import SvgIcon from '@/components/SvgIcon'
+import styles from './lawyerLegalKnowledge.module.scss'
+
 const LawyerLegalKnowledge = () => {
-  return <div>LawyerLegalKnowledge</div>
+  return (
+    <section className={styles['lawyer-legal-knowledge']} aria-label="변호사의 법률 지식">
+      <header className={styles['lawyer-legal-knowledge__header']}>
+        <h3 className={styles['lawyer-legal-knowledge__title']}>변호사의 법률 지식</h3>
+        <button type='button' className={styles['lawyer-legal-knowledge__button']} aria-label='변호사의 법률 지식 더보기'>
+          더보기
+          <SvgIcon name='arrowSmall' className={styles['lawyer-legal-knowledge__button-icon']} size={14} />
+        </button>
+      </header>
+      <Divider padding={14} />
+    </section>
+  )
 }
 
 export default LawyerLegalKnowledge
--- src/container/lawyer/lawyerVideo/LawyerVideo.tsx
+++ src/container/lawyer/lawyerVideo/LawyerVideo.tsx
@@ -1,5 +1,20 @@
+import Divider from '@/components/divider/Divider'
+import SvgIcon from '@/components/SvgIcon'
+import styles from './lawyerVideo.module.scss'
+
 const LawyerVideo = () => {
-  return <div>LawyerVideo</div>
+  return (
+    <section className={styles['lawyer-video']} aria-label="변호사의 영상">
+      <header className={styles['lawyer-video__header']}>
+        <h3 className={styles['lawyer-video__title']}>변호사의 영상</h3>
+        <button type='button' className={styles['lawyer-video__button']} aria-label='변호사의 영상 더보기'>
+          더보기
+          <SvgIcon name='arrowSmall' className={styles['lawyer-video__button-icon']} size={14} />
+        </button>
+      </header>
+      <Divider padding={14} />
+    </section>
+  )
 }
 
 export default LawyerVideo
