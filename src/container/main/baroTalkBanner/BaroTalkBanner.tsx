3a7a095e2fb277331a782e17e0bb2c11ff2e930a
MAZE-BORAHM\lucyl <amorosso@a-maze.ai>
2025-08-08 15:20:15 +0900

    feat: 마이페이지 채팅 기록 탭 추가 및 네비게이션 기능 개선

--- src/container/main/baroTalkBanner/BaroTalkBanner.tsx
+++ src/container/main/baroTalkBanner/BaroTalkBanner.tsx
@@ -10,11 +10,21 @@ const BaroTalkBanner = () => {
     navigate(ROUTER.REQUEST_BARO_TALK)
   }
 
+  const handleChatHistory = () => {
+    navigate(ROUTER.MYPAGE, {
+      state: {
+        tab: 'chatHistory',
+      },
+    })
+  }
+
   return (
     <div className={styles.container}>
       <h3 className={styles.title}>{`전문 변호사와\n채팅상담을 지금 바로 할 수 있습니다.`}</h3>
       <div className={styles['button-container']}>
-        <button className={styles.history}>법률 지식인 상담 내역</button>
+        <button className={styles.history} onClick={handleChatHistory}>
+          법률 지식인 상담 내역
+        </button>
         <button className={styles.chat} onClick={handleRequestBaroTalk}>
           변호사에게 채팅 상담
         </button>
--- src/pages/mypage/Mypage.tsx
+++ src/pages/mypage/Mypage.tsx
@@ -2,11 +2,13 @@ import ChatList from '@/container/mypage/myChatList/MyChatList'
 import KeepList from '@/container/mypage/keepList/KeepList'
 import MypageHeader from '@/container/mypage/mypageHeader/MypageHeader'
 import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const TABS = ['keepList', 'chatList']
 
 const Mypage = () => {
-  const [tab, setTab] = useState(TABS[0])
+  const { state } = useLocation()
+  const [tab, setTab] = useState(state?.tab || TABS[0])
   const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('desc')
 
   const handleTabClick = (tab: string) => {
