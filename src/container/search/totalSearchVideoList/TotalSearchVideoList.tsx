c0a6e7445d1f95457bb39e0d049ce670e52710f8
MAZE-BORAHM\lucyl <amorosso@a-maze.ai>
2025-08-09 10:45:59 +0900

    feat: 검색 결과 컴포넌트에 비디오 목록 추가 및 스타일 정의, VideoHorizon 컴포넌트 통합

--- src/container/search/totalSearchVideoList/TotalSearchVideoList.tsx
+++ src/container/search/totalSearchVideoList/TotalSearchVideoList.tsx
@@ -1,5 +1,8 @@
 import { VideoCase } from '@/types/videoTypes'
-// import { useNavigate } from 'react-router-dom'
+import SearchSectionHeader from '../searchSectionHeader/SearchSectionHeader'
+import styles from './totalSearchVideoList.module.scss'
+import VideoHorizon from '@/components/video/VideoHorizon'
+import { useNavigate } from 'react-router-dom'
 
 interface TotalSearchVideoListProps {
   searchResults: VideoCase[]
@@ -7,11 +10,43 @@ interface TotalSearchVideoListProps {
 }
 
 const TotalSearchVideoList = ({ searchResults, query }: TotalSearchVideoListProps) => {
-  // const navigate = useNavigate()
+  const navigate = useNavigate()
 
-  console.log('searchResults', searchResults, query)
+  const handleClickMore = () => {
+    navigate(`/search/video?query=${query}`)
+  }
 
-  return <div>TotalVideoList</div>
+  return (
+    <div className={styles['total-search-video-list']}>
+      <SearchSectionHeader title='법률 영상' onClickMore={handleClickMore} />
+      <div className={styles['video-list']}>
+        {searchResults.map(video => (
+          <div>
+            <VideoHorizon
+              key={video.videoCaseId}
+              videoCaseId={video.videoCaseId}
+              isKeep={video.isKeep}
+              size='small'
+              thumbnailUrl={video.thumbnail}
+              title={video.title}
+              summaryContents={video.summaryContent}
+            />
+            <footer className={styles['video-item-footer']}>
+              <span className={styles['lawyer-info']}>
+                {video.lawyerName} 변호사 [{video.lawfirmName}]
+              </span>
+              <div className={styles['channel-info']}>
+                <figure>
+                  <img src={video.thumbnail} alt={video.channelName} />
+                </figure>
+                <span className={styles['channel-name']}>{video.channelName}</span>
+              </div>
+            </footer>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default TotalSearchVideoList
