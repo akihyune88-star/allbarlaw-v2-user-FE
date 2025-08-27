4fd39f6b5a27761bb64980df5f888cc264f8874b
MAZE-BORAHM\lucyl <amorosso@a-maze.ai>
2025-08-27 16:45:36 +0900

    feat: LawyerVideoDetail 컴포넌트에서 사용하지 않는 코드 주석 처리 및 상태 변수 이름 변경

--- src/pages/lawyerAdmin/lawyerContents/lawyerVideo/lawyerVideoDetail/LawyerVideoDetail.tsx
+++ src/pages/lawyerAdmin/lawyerContents/lawyerVideo/lawyerVideoDetail/LawyerVideoDetail.tsx
@@ -1,9 +1,9 @@
 import styles from './lawyerVideoDetail.module.scss'
 import { useParams, useNavigate } from 'react-router-dom'
 import { useGetVideoDetail } from '@/hooks/queries/useGetVideoDetail'
-import Button from '@/components/button/Button'
-import SvgIcon from '@/components/SvgIcon'
-import { COLOR } from '@/styles/color'
+// import Button from '@/components/button/Button'
+// import SvgIcon from '@/components/SvgIcon'
+// import { COLOR } from '@/styles/color'
 import { copyUrlToClipboard } from '@/utils/clipboard'
 import { useVideoKeep } from '@/hooks/queries/useGetVideoList'
 import { useState } from 'react'
@@ -16,38 +16,38 @@ import YoutubePlayer from '@/components/youtubePlayer/YoutubePlayer'
 import { Tag } from '@/types/lawyerTypes'
 import VidoeInfo from '@/container/video/VidoeInfo'
 
-type VideoNavigationBarProps = {
-  isKeep: boolean
-  onSave: () => void
-  onShare: () => void
-  onVideoLink: () => void
-}
+// type VideoNavigationBarProps = {
+//   isKeep: boolean
+//   onSave: () => void
+//   onShare: () => void
+//   onVideoLink: () => void
+// }
 
-const VideoNavigationBar = ({ isKeep, onSave, onShare, onVideoLink }: VideoNavigationBarProps) => {
-  return (
-    <div className={styles['video-navigation-bar']}>
-      <button className={styles['video-link-btn']} onClick={onVideoLink}>
-        영상 바로가기
-      </button>
-      <div className={styles['button-wrapper']}>
-        <Button variant='share' onClick={onShare}>
-          공유
-          <SvgIcon name='share' size={16} />
-        </Button>
-        <Button variant='save' onClick={onSave}>
-          저장 <SvgIcon name='save' size={16} fill={isKeep ? COLOR.icon_darkgreen : 'none'} />
-        </Button>
-      </div>
-    </div>
-  )
-}
+// const VideoNavigationBar = ({ isKeep, onSave, onShare, onVideoLink }: VideoNavigationBarProps) => {
+//   return (
+//     <div className={styles['video-navigation-bar']}>
+//       <button className={styles['video-link-btn']} onClick={onVideoLink}>
+//         영상 바로가기
+//       </button>
+//       <div className={styles['button-wrapper']}>
+//         <Button variant='share' onClick={onShare}>
+//           공유
+//           <SvgIcon name='share' size={16} />
+//         </Button>
+//         <Button variant='save' onClick={onSave}>
+//           저장 <SvgIcon name='save' size={16} fill={isKeep ? COLOR.icon_darkgreen : 'none'} />
+//         </Button>
+//       </div>
+//     </div>
+//   )
+// }
 
 const LawyerVideoDetail = () => {
   const { videoCaseId } = useParams<{ videoCaseId: string }>()
   const navigate = useNavigate()
   const { data } = useGetVideoDetail({ videoCaseId: Number(videoCaseId) })
   const { data: lawyerBasicInfo } = useLawyerDetailForMe()
-  const [isKeep, setIsKeep] = useState(false)
+  const [_isKeep, setIsKeep] = useState(false)
 
   const { mutate: changeVideoKeep } = useVideoKeep({
     onSuccess: data => {
@@ -59,20 +59,20 @@ const LawyerVideoDetail = () => {
     },
   })
 
-  const handleShare = () => {
-    copyUrlToClipboard()
-  }
+  // const handleShare = () => {
+  //   copyUrlToClipboard()
+  // }
 
-  const handleSave = () => {
-    if (data?.videoCaseId) {
-      setIsKeep(prevState => !prevState)
-      changeVideoKeep(data.videoCaseId)
-    }
-  }
+  // const handleSave = () => {
+  //   if (data?.videoCaseId) {
+  //     setIsKeep(prevState => !prevState)
+  //     changeVideoKeep(data.videoCaseId)
+  //   }
+  // }
 
-  const handleVideoLink = () => {
-    window.open(data?.source || '', '_blank')
-  }
+  // const handleVideoLink = () => {
+  //   window.open(data?.source || '', '_blank')
+  // }
 
   const handleExcelUpload = () => {
     console.log('ExcelUpload')
@@ -116,8 +116,6 @@ const LawyerVideoDetail = () => {
           source={data?.source || ''}
         />
         <VideoSummary summary={data?.summaryContent || ''} className={styles['lawyer-video-detail-summary']} />
-
-        {/* <VideoNavigationBar isKeep={isKeep} onSave={handleSave} onShare={handleShare} onVideoLink={handleVideoLink} /> */}
       </div>
     </>
   )
