15d4a0d7c3a4216277893ab6776acb6007bff337
MAZE-Borahm <amorosso@a-maze.ai>
2025-09-24 11:35:38 +0900

    refactor: 변호사 어드민 법률 지식인 헤더 문구 수정

--- src/pages/lawyerAdmin/lawyerContents/lawyerLegalKnowledge/lawyerLegalKnowledgeDetail/LawyerLegalKnowledgeDetail.tsx
+++ src/pages/lawyerAdmin/lawyerContents/lawyerLegalKnowledge/lawyerLegalKnowledgeDetail/LawyerLegalKnowledgeDetail.tsx
@@ -1,21 +1,20 @@
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { useGetKnowledgeDetail } from '@/hooks/queries/useGetKnowledgeDetail'
 import styles from './lawyerLegalKnowledgeDetail.module.scss'
 import HeaderPortal from '@/components/headerPortal/HeaderPortal'
-import { ROUTER } from '@/routes/routerConstant'
 import ConsultationContentCard from '@/components/consultationContentCard/ConsultationContentCard'
 import { getRelativeTimeString } from '@/utils/date'
 import DetailHeader from '@/components/detailHeader/DetailHeader'
 import LawyerResponse from '@/container/legalKnowledge/LawyerResponse'
+import { useLawyerDetailForMe } from '@/hooks/queries/useLawyer'
 
 const LawyerLegalKnowledgeDetail = () => {
   const { knowledgeId } = useParams<{ knowledgeId: string }>()
-  const navigate = useNavigate()
-  const { data, isLoading } = useGetKnowledgeDetail({ knowledgeId: Number(knowledgeId) })
+  const { data: lawyerDetailForMe } = useLawyerDetailForMe()
 
-  const handleGoBack = () => {
-    navigate(ROUTER.LAWYER_ADMIN_CONTENT_LEGAL_KNOWLEDGE)
-  }
+  const lawyerName = lawyerDetailForMe?.lawyerName || ''
+
+  const { data, isLoading } = useGetKnowledgeDetail({ knowledgeId: Number(knowledgeId) })
 
   if (isLoading) {
     return <div className={styles['loading']}>로딩 중...</div>
@@ -25,10 +24,7 @@ const LawyerLegalKnowledgeDetail = () => {
     <>
       <HeaderPortal>
         <div className={styles.headerContent}>
-          <button className={styles.backButton} onClick={handleGoBack}>
-            ← 목록으로
-          </button>
-          <h1 className={styles.headerTitle}>법률 지식인 상세</h1>
+          <h1 className={styles.headerTitle}>{lawyerName} 변호사를 선택한 법률 지식인 목록입니다. </h1>
         </div>
       </HeaderPortal>
       <div className={styles['legal-knowledge-detail']}>
