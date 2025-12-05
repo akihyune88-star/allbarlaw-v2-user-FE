6669f34ca0df330175d1bdb9675d3ff76e9a36a4
MAZE-BORAHM\lucyl <amorosso@a-maze.ai>
2025-12-05 11:01:45 +0900

    refactor: UserAboutFaqList 컴포넌트에서 Accordion을 PaginatedFaqList로 교체
    - FAQ 표시 개선을 위해 Accordion 컴포넌트를 제거하고 PaginatedFaqList 통합
    - 사용하지 않는 탭 및 관련 로직을 제거하여 코드 간소화 및 유지보수성 향상
    - 페이지네이션 지원을 위한 FAQ 렌더링 업데이트로 사용자 경험 개선

--- src/container/aboutAllbarlaw/userAboutFaqList/UserAboutFaqList.tsx
+++ src/container/aboutAllbarlaw/userAboutFaqList/UserAboutFaqList.tsx
@@ -1,57 +1,12 @@
-import { Accordion } from '@/components/accordion'
-import Divider from '@/components/divider/Divider'
-import Tabs from '@/components/tabs/Tabs'
-import React from 'react'
 import styles from './userAboutFaqList.module.scss'
 import { USER_FAQ } from '@/constants/userFaq'
-
-const tabs = [
-  {
-    faqTypeId: 1,
-    path: 'all',
-    name: '전체',
-  },
-  // {
-  //   faqTypeId: 2,
-  //   path: 'user',
-  //   name: '사용자',
-  // },
-  // {
-  //   faqTypeId: 3,
-  //   path: 'lawyer',
-  //   name: '변호사',
-  // },
-]
+import PaginatedFaqList from '@/components/faqList/PaginatedFaqList'
 
 const UserAboutFaqList = () => {
-  const getFaqTypeName = (faqTypeId: number) => {
-    return tabs.find((tab: any) => tab.faqTypeId === faqTypeId)?.name
-  }
   return (
     <div className={styles['user-about-faq-list']}>
       <h2 className={styles['section-title']}>자주묻는 질문</h2>
-      {/* <Tabs items={tabs} onChange={() => {}} initialPath='all' className={styles['tabs']} /> */}
-      <section className={styles['faq-list']}>
-        <Accordion allowMultiple={true}>
-          {USER_FAQ.map((faq: any, index: number) => (
-            <React.Fragment key={faq.faqId}>
-              <Accordion.Item id={faq.faqId}>
-                <Accordion.Title itemId={faq.faqId}>
-                  <div className={styles['faq-title']}>
-                    {/* <strong>{getFaqTypeName(faq.faqTypeId)}</strong> */}
-                    <strong>FAQ</strong>
-                    <span className={styles['faq-title-text']}>{faq.faqTitle}</span>
-                  </div>
-                </Accordion.Title>
-                <Accordion.Content itemId={faq.faqId}>
-                  <div className={styles['faq-content']}>{faq.faqContent}</div>
-                </Accordion.Content>
-              </Accordion.Item>
-              {index !== USER_FAQ.length - 1 && <Divider padding={16} />}
-            </React.Fragment>
-          ))}
-        </Accordion>
-      </section>
+      <PaginatedFaqList faqData={USER_FAQ} itemsPerPage={5} />
     </div>
   )
 }
