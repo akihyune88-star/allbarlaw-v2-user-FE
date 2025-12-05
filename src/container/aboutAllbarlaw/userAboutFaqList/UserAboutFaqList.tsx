72ebc47bea40cc42c12ba78b58d6a03c3c71ec2f
MAZE-BORAHM\lucyl <amorosso@a-maze.ai>
2025-12-05 10:00:46 +0900

    refactor: FAQ 처리 및 스타일링 업데이트
    - 유지보수성 향상을 위해 하드코딩된 FAQ 목록을 USER_FAQ 상수로 교체
    - 코드 간소화를 위해 사용하지 않는 FAQ 항목 및 탭 주석 처리
    - 가독성과 일관성 향상을 위해 FAQ 콘텐츠 스타일 조정

--- src/container/aboutAllbarlaw/userAboutFaqList/UserAboutFaqList.tsx
+++ src/container/aboutAllbarlaw/userAboutFaqList/UserAboutFaqList.tsx
@@ -3,21 +3,7 @@ import Divider from '@/components/divider/Divider'
 import Tabs from '@/components/tabs/Tabs'
 import React from 'react'
 import styles from './userAboutFaqList.module.scss'
-
-const faqList = [
-  {
-    faqId: 1,
-    faqTypeId: 1,
-    faqTitle: 'FAQ 1',
-    faqContent: 'FAQ 1 내용',
-  },
-  {
-    faqId: 2,
-    faqTypeId: 2,
-    faqTitle: 'FAQ 2',
-    faqContent: 'FAQ 2 내용',
-  },
-]
+import { USER_FAQ } from '@/constants/userFaq'
 
 const tabs = [
   {
@@ -25,16 +11,16 @@ const tabs = [
     path: 'all',
     name: '전체',
   },
-  {
-    faqTypeId: 2,
-    path: 'user',
-    name: '사용자',
-  },
-  {
-    faqTypeId: 3,
-    path: 'lawyer',
-    name: '변호사',
-  },
+  // {
+  //   faqTypeId: 2,
+  //   path: 'user',
+  //   name: '사용자',
+  // },
+  // {
+  //   faqTypeId: 3,
+  //   path: 'lawyer',
+  //   name: '변호사',
+  // },
 ]
 
 const UserAboutFaqList = () => {
@@ -44,15 +30,16 @@ const UserAboutFaqList = () => {
   return (
     <div className={styles['user-about-faq-list']}>
       <h2 className={styles['section-title']}>자주묻는 질문</h2>
-      <Tabs items={tabs} onChange={() => {}} initialPath='all' className={styles['tabs']} />
+      {/* <Tabs items={tabs} onChange={() => {}} initialPath='all' className={styles['tabs']} /> */}
       <section className={styles['faq-list']}>
         <Accordion allowMultiple={true}>
-          {faqList.map((faq: any, index: number) => (
+          {USER_FAQ.map((faq: any, index: number) => (
             <React.Fragment key={faq.faqId}>
               <Accordion.Item id={faq.faqId}>
                 <Accordion.Title itemId={faq.faqId}>
                   <div className={styles['faq-title']}>
-                    <strong>{getFaqTypeName(faq.faqTypeId)}</strong>
+                    {/* <strong>{getFaqTypeName(faq.faqTypeId)}</strong> */}
+                    <strong>FAQ</strong>
                     <span className={styles['faq-title-text']}>{faq.faqTitle}</span>
                   </div>
                 </Accordion.Title>
@@ -60,7 +47,7 @@ const UserAboutFaqList = () => {
                   <div className={styles['faq-content']}>{faq.faqContent}</div>
                 </Accordion.Content>
               </Accordion.Item>
-              {index !== faqList.length - 1 && <Divider padding={16} />}
+              {index !== USER_FAQ.length - 1 && <Divider padding={16} />}
             </React.Fragment>
           ))}
         </Accordion>
